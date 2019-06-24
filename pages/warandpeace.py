75ca5e7d480f65dbdbaad1b42e23a982d2bd9c3e
andriTinas <andrianustinas.siskom@gmail.com>
2019-06-24 13:47:17 +0700

    6

--- pages/warandpeace.py
+++ pages/warandpeace.py
@@ -4,7 +4,23 @@ from bs4 import BeautifulSoup
 html = urlopen("http://pythonscraping.com/pages/warandpeace.html")
 
 bsObj=BeautifulSoup(html , 'html.parser')
-nameList = bsObj.findAll("span",{"class":"green"})
+
+'''
+nameList = bsObj.findAll("span",{"class":{"green","red"}})
 
 for name in nameList :
     print(name.get_text())
+
+print(len(nameList))
+
+nameList = bsObj.findAll(text="the prince")
+print(len(nameList))
+
+nameList = bsObj.findAll({'h1','h2','h3','h4','h5','h6'})
+for name in nameList :
+    print(name.get_text())
+
+nameList = bsObj.findAll(id="title")
+for name in nameList :
+    print(name.get_text())
+'''
