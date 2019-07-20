463cfe68c92ea0d4ab45b89ed75bd9ca5c42c218
andriTinas <andrianustinas.siskom@gmail.com>
2019-07-20 21:39:26 +0700

    15-How-APIs-Work-16-Communicating-with-APIs

---
+++ twitter/client.py
@@ -0,0 +1,22 @@
+import sys
+from tweepy import OAuthHandler
+ 
+def get_twitter_auth():
+
+    try:
+        consumer_key    = ''
+        consumer_secret = ''
+        access_token    = ''
+        access_secret   = ''
+    except KeyError:
+        sys.stderr.write("TWITTER_* enveroment variables not set\n")
+        sys.exit(1)
+
+    auth = OAuthHandler(consumer_key,consumer_secret)
+    auth.set_access_token(access_token,access_secret)
+    return auth
+
+def get_twitter_client():
+    auth    = get_twitter_auth()
+    client  = API(auth)
+    return client
